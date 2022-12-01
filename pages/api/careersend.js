34d026dd996f29a7aa72d15018131a92ee4773a1
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2022-12-01 06:08:47 +0000

    file upload attempt

--- pages/api/careersend.js
+++ pages/api/careersend.js
@@ -1,35 +1,85 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
 import nodemailer from "nodemailer";
+import multer from "multer";
 
 export default async function handler(req, res) {
-  const { careerName, careerEmail, careerMessage, careerTelephone } = req.body;
-
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.user,
-      pass: process.env.pass,
+  let Storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, "../../files");
+    },
+    filename: (req, file, callback) => {
+      console.log(file);
+      callback(
+        null,
+        file.fieldname + "_" + Date.now() + "_" + file.originalname
+      );
     },
   });
 
-  try {
-    const emailRes = await transporter.sendMail({
-      from: `${giftContactName} <${giftContactEmail}>`,
-      to: "deanparkim1987@gmail.com",
-      subject: `Contact Form Submission from ${giftContactName}`,
-      html: `<p>You have a new contact form submission</p><br>
-      <p><strong>Name: </strong> ${giftContactName}</p><br>
-      <p><strong>Email: </strong> ${giftContactEmail}</p><br>
-      <p><strong>Phone Number: </strong> ${giftContactPhoneNumber}</p><br>
-      <p><strong>Amount: </strong> ${amount}</p><br>
-      <p><strong>Message: </strong> ${giftContactMessage}</p>`,
-    });
-    console.log("Message sent: %s", emailRes.messageId);
-  } catch (error) {
-    console.error(error);
-  }
+  let upload = multer({
+    storage: Storage,
+  }).single("careerCV");
+
+  upload(req, res, function (err) {
+    if (err) {
+      console.log(err);
+      return res.end("something went wrong");
+    } else {
+      var {
+        careerName,
+        careerEmail,
+        careerMessage,
+        careerTelephone,
+        careerBadge,
+        careerCV,
+      } = req.body;
+      console.log(careerName);
+      console.log(careerEmail);
+      console.log(careerMessage);
+      console.log(careerTelephone);
+      console.log(careerBadge);
+      console.log(careerCV);
+    }
+  });
+
+  // const {
+  //   careerName,
+  //   careerEmail,
+  //   careerMessage,
+  //   careerTelephone,
+  //   careerBadge,
+  //   careerCV,
+  // } = req.body;
+
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: process.env.user,
+  //     pass: process.env.pass,
+  //   },
+  // });
+
+  // try {
+  //   const emailRes = await transporter.sendMail({
+  //     from: `${careerName} <${careerEmail}>`,
+  //     replyTo: `${careerName} <${careerEmail}>`,
+  //     to: "deanparkim1987@gmail.com",
+  //     subject: `Career Form Submission from ${careerName}`,
+  //     html: `<p>You have a new Career form submission</p><br>
+  //     <p><strong>Name: </strong> ${careerName}</p><br>
+  //     <p><strong>Email: </strong> ${careerEmail}</p><br>
+  //     <p><strong>Phone Number: </strong> ${careerTelephone}</p><br>
+  //     <p><strong>Badge Number: </strong> ${careerBadge}</p><br>
+  //     <p><strong>Message: </strong> ${careerMessage}</p>`,
+  //     attachments: careerCV,
+  //   });
+  //   console.log("Message sent: %s", emailRes.messageId);
+  // } catch (error) {
+  //   console.error(error);
+  // }
 
   console.log(req.body);
   res.status(200).json(req.body);
