573e62ec753b1daa1d503437e9e34ac40534006a
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2022-12-04 12:48:33 +0000

    styling update

--- pages/index.js
+++ pages/index.js
@@ -29,16 +29,40 @@ export default function Home({ feed }) {
         description="A formal friendly door security Firm, aimed to raise the standard in the security industry."
       />
       {/* <Carousel /> */}
-      <header className="text-light text-center my-5">
-        <h2 className="fs-5 baskerville-font">
-          Welcome To <br />
-          <span className="h1 text-primary">Bevington Security Solutions</span>
-        </h2>
+      <header className="text-light text-center">
+        <div className="hero-wrapper">
+          <Image
+            src={heroImage}
+            alt="Bevington Security Door Team"
+            class="hero-image"
+          />
+          <div className="hero-text d-none d-md-block">
+            <h2 className="fs-5 baskerville-font">
+              Welcome To <br />
+              <span className="h1 text-primary">
+                Bevington Security Solutions
+              </span>
+            </h2>
 
-        <p className="my-2">
-          A formal friendly door security Firm, aimed to raise the standard in
-          the security industry.
-        </p>
+            <p className="my-2">
+              A formal friendly door security Firm, aimed to raise the standard
+              in the security industry.
+            </p>
+          </div>
+        </div>
+        <div className="d-block d-md-none">
+          <h2 className="fs-5 baskerville-font">
+            Welcome To <br />
+            <span className="h1 text-primary">
+              Bevington Security Solutions
+            </span>
+          </h2>
+
+          <p className="my-2">
+            A formal friendly door security Firm, aimed to raise the standard in
+            the security industry.
+          </p>
+        </div>
       </header>
       <main>
         <section>
--- pages/services.js
+++ pages/services.js
@@ -8,7 +8,7 @@ export default function Services() {
         title="Services"
         description="A formal friendly door security Firm, aimed to raise the standard in the security industry."
       />
-      <section className="container">
+      <section>
         <ServicesComp />
       </section>
     </>
