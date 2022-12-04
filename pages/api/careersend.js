8e3755cd82dbb90fc78b6cb8315515100aa54ab7
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2022-12-04 16:26:05 +0000

    smtp update

--- pages/api/careersend.js
+++ pages/api/careersend.js
@@ -89,7 +89,7 @@ export default async function handler(req, res) {
 
     //Email Transporter Settings
     const emailRes = await transporter.sendMail({
-      from: `No-reply <site@bevingtonsecurity.com>`,
+      from: `Bevington-Security <site@bevingtonsecurity.com>`,
       replyTo: `${careerName} <${careerEmail}>`,
       to: "deanparkim1987@gmail.com",
       subject: `Career Form Submission from ${careerName}`,
